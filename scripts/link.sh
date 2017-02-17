0e22ca23fa71bbc2cbd8ebe2b868dd72fb3fe351
Omar Polo <yum1096@gmail.com>
2017-02-17 13:51:00 +0100

    New script for the right side of the tmux status bar

--- scripts/link.sh
+++ scripts/link.sh
@@ -3,5 +3,6 @@
 mkdir -p ~/.local/bin/
 
 ln -sv `pwd`/google ~/.local/bin/google
+ln -sv `pwd`/tmux-right-side ~/.local/bin/tmux-right-side
 ln -sv `pwd`/shared ~/.local/bin/shared
 ln -sv `pwd`/update_pkg ~/.local/bin/update_pkg
